9520232e7adfed14afd1b52f496833265c8119f5
Zdunn-Design <zdunn16934@gmail.com>
2024-03-09 16:02:07 -0500

    Final updates

--- main.py
+++ main.py
@@ -2,9 +2,19 @@ def main():
     book_path ='books/frankenstein.txt'
     texts = get_book_text(book_path)
     word_count = word_counter(texts)
-    print(f"word count comes to {word_count}")
     letter_dict = letter_counter(texts)
-    print(letter_dict)
+    letter_list_sorted = dict_to_sorted_list(letter_dict)
+
+    print(f"--- Start Report of {book_path} ---")
+    print()
+    print(f"Word count comes to {word_count}")
+    print()
+    
+    for item in letter_list_sorted:
+        if item['char'].isalpha():
+            print(f"The '{item['char']}' character was found {item['num']} times")
+    print()
+    print(f"--- END OF REPORT ---")    
 
 def get_book_text(path):
     with open(path) as f:
@@ -14,6 +24,16 @@ def word_counter(text):
     words = text.split()
     return len(words)
 
+def sort_on(d):
+    return d["num"]
+
+def dict_to_sorted_list(count_dict):
+    sorted_list = []
+    for ch in count_dict:
+        sorted_list.append({"char": ch, "num": count_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
 def letter_counter(text):
     characters = {}
     for x in text:
@@ -22,6 +42,7 @@ def letter_counter(text):
             characters[low] += 1
         else:
             characters[low] = 1
+    #print(characters)
     return characters
 
 if __name__ == '__main__':
